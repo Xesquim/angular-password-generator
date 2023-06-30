9ce666d61fef2cbe110e0bd2af932231fb4633aa
Xesquim <matheus.g.cesquim@gmail.com>
2023-06-30 03:04:03 -0300

    Creating function to generate password

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -39,5 +39,29 @@ export class AppComponent {
     this.generate();
   }
 
-  public generate(): void {}
+  public generate(): void {
+    if (!this.passwordForm.valid) return;
+
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%&*()';
+
+    let validChars = '';
+    let generatedPassword = '';
+
+    if (this.passwordForm.controls['useLetters'].value) validChars += letters;
+    if (this.passwordForm.controls['useNumbers'].value) validChars += numbers;
+    if (this.passwordForm.controls['useSymbols'].value) validChars += symbols;
+
+    for (let i = 0; i <= this.passwordForm.controls['length'].value; i++) {
+      var mix = validChars.charAt(
+        Math.floor(Math.random() * validChars.length)
+      );
+      if (mix.match(/[a-z]/i) && Math.random() < 0.5) {
+        mix = mix.toUpperCase();
+      }
+      generatedPassword += mix;
+    }
+    this.passwordForm.controls['generatedPassword'].setValue(generatedPassword);
+  }
 }
